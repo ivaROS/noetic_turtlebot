b6e279368fb426759958fd6ece5a182347ca5292
Justin Smith <56697793+drjsmith@users.noreply.github.com>
2023-01-22 18:22:20 -0500

    Update install.sh
    Don't prompt when installing packages

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install curl
+sudo apt install curl -y
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
-sudo apt install ros-noetic-desktop-full \
+sudo apt install -y ros-noetic-desktop-full \
 python3-catkin-tools python3-wstool python3-rosdep python3-rosinstall python3-rosinstall-generator
 
 cd /opt/ros
