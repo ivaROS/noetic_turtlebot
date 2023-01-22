18fb6fbb91f0ba34a9b44a4711ba4bef7ec33c02
Justin Smith <56697793+drjsmith@users.noreply.github.com>
2023-01-22 18:15:17 -0500

    Update install.sh
    Ensure base ROS install is present; change workspace name; install dependencies with rosdep; move 'sourcing' command to correct file

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
-sudo apt install python3-catkin-tools python3-wstool python3-rosdep python3-rosinstall python3-rosinstall-generator build-essential libftdi-dev pyqt5-dev-tools ros-noetic-openslam-gmapping ros-noetic-navigation ros-noetic-joy ros-noetic-depthimage-to-laserscan ros-noetic-kobuki-msgs ros-noetic-ecl-threads ros-noetic-ecl-formatters ros-noetic-ecl-streams ros-noetic-ecl-linear-algebra ros-noetic-ecl-geometry ros-noetic-kobuki-dock-drive ros-noetic-kobuki-driver
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt install curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update
+sudo apt install ros-noetic-desktop-full \
+python3-catkin-tools python3-wstool python3-rosdep python3-rosinstall python3-rosinstall-generator
 
 cd /opt/ros
-sudo mkdir catkin_ws && sudo chown -R $USER catkin_ws
-cd catkin_ws
+sudo mkdir -p ivalab && sudo chown -R $USER ivalab
+cd ivalab
 catkin init
 catkin config --mkdirs --extend /opt/ros/noetic --cmake-args -DCMAKE_BUILD_TYPE="Release" -DCMAKE_CXX_FLAGS_RELEASE="-O2 -g" -DCMAKE_C_FLAGS_RELEASE="-O2 -g"
-wstool init src https://raw.githubusercontent.com/ivaROS/noetic_turtlebot/main/rosinstall -j20
+wstool init
+wstool merge -t src https://raw.githubusercontent.com/ivaROS/noetic_turtlebot/main/rosinstall 
+wstool update -j20
+
+sudo rosdep init
+rosdep update
+source /opt/ros/noetic/setup.bash
+rosdep install --from-paths src --ignore-src -y -r
+
 catkin build
-sudo bash -c "echo 'source /opt/ros/catkin_ws/devel/setup.bash' > /etc/profile.d/ros-noetic-turtlebot.sh"
+sudo bash -c "echo 'source /opt/ros/ivalab/devel/setup.bash' > /etc/bash.bashrc"
