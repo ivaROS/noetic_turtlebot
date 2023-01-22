27e44a2b48c17615748aff72f74325381fca27c7
Justin Smith <56697793+drjsmith@users.noreply.github.com>
2023-01-22 18:25:40 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo mkdir -p ivalab && sudo chown -R $USER ivalab
 cd ivalab
 catkin init
 catkin config --mkdirs --extend /opt/ros/noetic --cmake-args -DCMAKE_BUILD_TYPE="Release" -DCMAKE_CXX_FLAGS_RELEASE="-O2 -g" -DCMAKE_C_FLAGS_RELEASE="-O2 -g"
-wstool init
+wstool init src
 wstool merge -t src https://raw.githubusercontent.com/ivaROS/noetic_turtlebot/main/rosinstall 
 wstool update -j20
 
