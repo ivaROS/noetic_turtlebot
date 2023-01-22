fe6e6e087193bc1a94bb4badeaa4b6b36b38f097
Justin Smith <56697793+drjsmith@users.noreply.github.com>
2023-01-22 18:29:59 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ catkin init
 catkin config --mkdirs --extend /opt/ros/noetic --cmake-args -DCMAKE_BUILD_TYPE="Release" -DCMAKE_CXX_FLAGS_RELEASE="-O2 -g" -DCMAKE_C_FLAGS_RELEASE="-O2 -g"
 wstool init src
 wstool merge -t src https://raw.githubusercontent.com/ivaROS/noetic_turtlebot/main/rosinstall 
-wstool update -j20
+wstool update -t src -j20
 
 sudo rosdep init
 rosdep update
