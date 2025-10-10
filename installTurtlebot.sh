6d0b57771a265a2aac37bf63dea6e952d30420df
Patricio A. Vela <pvela@gatech.edu>
2025-10-10 12:46:09 -0400

    Adjusting install with patches.

--- installTurtlebot.sh
+++ installTurtlebot.sh
@@ -1,15 +1,22 @@
 
+OWD=`pwd`
+
 # Makes an extended workspace under assumption that base workspace is in /opt/ros/noetic
 cd /opt/ros
 sudo mkdir -p ivalab && sudo chown -R $USER ivalab
 cd ivalab
 catkin init
-catkin config --mkdirs --extend /opt/ros/noetic --cmake-args -DCMAKE_BUILD_TYPE="Release" -DCMAKE_CXX_FLAGS_RELEASE="-O2 -g" -DCMAKE_C_FLAGS_RELEASE="-O2 -g"
+catkin config --mkdirs --extend /opt/ros/noetic --cmake-args -DCMAKE_BUILD_TYPE="Release" \
+		-DCMAKE_CXX_FLAGS_RELEASE="-O2 -g" -DCMAKE_C_FLAGS_RELEASE="-O2 -g"
 
 wstool init src
 wstool merge -t src https://raw.githubusercontent.com/ivaROS/noetic_turtlebot/main/noetic-additional.rosinstall 
 wstool merge -t src https://raw.githubusercontent.com/ivaROS/noetic_turtlebot/main/turtlebot.rosinstall 
 wstool update -t src -j20
+
+patch src/ros_gz/ros_ignition_gazebo < $OWD/patches/ros_ignition_gazebo.patch
+patch src/ros_gz/ros_ignition_bridge < $OWD/patches/ros_ignition_bridge.patch
+patch src/ros_gz/ros_ignition_image < $OWD/patches/ros_ignition_image.patch
 
 sudo rosdep init
 rosdep update
